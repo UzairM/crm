1aa81963b9bde3df4c93def4c86422424c9ca193
Uzair Mahmud <uzairmahmud@gmail.com>
2025-01-25 22:57:10 +0500

    changed crm

--- app/src/lib/ory.ts
+++ app/src/lib/ory.ts
@@ -3,7 +3,7 @@ import { Configuration, FrontendApi } from '@ory/client'
 // Initialize the ORY SDK with the base URL
 const ory = new FrontendApi(
   new Configuration({
-    basePath: import.meta.env.VITE_ORY_BASE_URL || 'http://localhost:4000',
+    basePath: import.meta.env.VITE_ORY_SDK_URL || 'http://localhost:4000',
     baseOptions: {
       withCredentials: true,
     },
